3ab054ec540dae75140f73b09d0a05bc8d63cbed
Ameir Abdeldayem <ameirh@gmail.com>
2017-05-09 04:01:32 -0400

    Include config file from CentOS 7.

--- files/scripts/after-install.sh
+++ files/scripts/after-install.sh
@@ -9,9 +9,9 @@ mkdir -m 0755 -vp /var/run/unbound
 chown unbound:unbound /var/run/unbound
 
 if pidof systemd; then
-    mv -v /tmp/unbound/systemd/unbound.service /usr/lib/systemd/system/unbound.service
+    mv -v /tmp/unbound/scripts/systemd/unbound.service /usr/lib/systemd/system/unbound.service
     echo "D /var/run/unbound 0755 unbound unbound -" > /usr/lib/tmpfiles.d/unbound.conf
     systemctl daemon-reload
 else
-    mv -v /tmp/unbound/sysvinit/unbound.sysvinit /etc/init.d/unbound
+    mv -v /tmp/unbound/scripts/sysvinit/unbound.sysvinit /etc/init.d/unbound
 fi
