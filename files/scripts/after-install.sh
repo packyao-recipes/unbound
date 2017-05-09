74e3489babca1da9bf485dac84b0caa89c3c38b2
Ameir Abdeldayem <ameirh@gmail.com>
2017-05-09 05:20:57 -0400

    Run `unbound-control-setup`.

--- files/scripts/after-install.sh
+++ files/scripts/after-install.sh
@@ -8,6 +8,9 @@ getent passwd unbound >/dev/null || \
 mkdir -m 0755 -vp /var/run/unbound
 chown unbound:unbound /var/run/unbound
 
+# create control keys
+unbound-control-setup
+
 if pidof systemd; then
     mv -v /tmp/unbound/scripts/systemd/unbound.service /usr/lib/systemd/system/unbound.service
     echo "D /var/run/unbound 0755 unbound unbound -" > /usr/lib/tmpfiles.d/unbound.conf
